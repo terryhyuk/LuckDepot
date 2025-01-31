6aed445d587f7a1cc94575744ac24a6a4961e662
Leewonyoung <wylee99@naver.com>
2025-01-31 17:16:21 +0900

    category_all 추가

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ from router.login import router as login_router
 from router.deliver import router as deliver_router
 from router.driver import router as driver_router
 from router.hub import router as hub_router
+from router.category import router as category_router
 
 static_dir = "../crawiling_img"
 API_KEY_HEADER = APIKeyHeader(name="Authorization", auto_error=False)
@@ -60,6 +61,7 @@ app.include_router(login_router, tags=["Login"], prefix="/login")
 app.include_router(deliver_router, tags=["Deliver"], prefix="/deliver")
 app.include_router(hub_router, tags=["Hub"], prefix="/hub")
 app.include_router(driver_router, tags=["Driver"], prefix="/driver")
+app.include_router(category_router, tags=["Category"], prefix="/category")
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
---
+++ router/category.py
@@ -0,0 +1,34 @@
+from database.model.product import Product
+from database.model.category import Category
+from fastapi import APIRouter, Depends, HTTPException, UploadFile, File
+from sqlalchemy.orm import Session
+from database.conn.connection import db
+from starlette.responses import FileResponse
+from errors import exceptions as ex
+# from static.models import 
+import os, shutil
+
+UPLOAD_FOLDER = 'crawling_img'
+
+router = APIRouter()
+
+"""
+    get : 조회 // status:200
+    post : 생성, 개인정보 조회 // status:201
+    put : 수정 //    기존 리소스가 수정된 경우: 200 (OK)
+                    전송 성공 후 자원이 생성됬을 경우: 201 (Created)
+                    전송은 했지만 전송할 데이터가 없는 경우: 204 (No Content)
+    delete : 삭제
+"""
+
+# 모든 상품 가져오는 API
+@router.get("/", status_code=200)
+async def index(session: Session = Depends(db.session)):
+    """
+    `All Category`\n
+    DB에 저장된 모든 Category 정보 가져오기 \n
+    :return:
+    """
+    category = session.query(Category).all()
+    return {"result" :category}
+
