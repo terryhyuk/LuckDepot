cd5a24ed05b6552190f7a4d4af9640eded3788ce
ppochacco <wsjeong0608@gmail.com>
2025-02-04 15:50:44 +0900

    postal_code dic 수정
    NearestDict

--- router/ml_model.py
+++ router/ml_model.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter, Depends
-from pydantic import BaseModel
+# from pydantic import BaseModel
 from joblib import load
 # from datetime import datetime, date
 import router.deliver as deliver
 from database.conn.connection import db
 from sqlalchemy.orm import Session
+from sortedcollections import NearestDict
 
 router = APIRouter()
 
@@ -12,27 +13,27 @@ router = APIRouter()
 model = load('./machine_learning/model/cluster_ny_gb.joblib')
 # Feature: [['Ship Mode', 'year', 'month', 'Weekday', 'num_Postal Code', 'Hurricane', 'BadWeather']]
 
-code_dic = {'10035': 3,
-        '10024': 2,
-        '10009': 0,
-        '10011': 1,
-        '14609': 19,
-        '11561': 9,
-        '11572': 10,
-        '13021': 13,
-        '10701': 5,
-        '12180': 12,
-        '11550': 8,
-        '13601': 16,
-        '14215': 17,
-        '10550': 4,
-        '13501': 15,
-        '11520': 7,
-        '10801': 6,
-        '13440': 14,
-        '14304': 18,
-        '14701': 20,
-        '11757': 11}
+code_dic = NearestDict({10035: 3,
+        10024: 2,
+        10009: 0,
+        10011: 1,
+        14609: 19,
+        11561: 9,
+        11572: 10,
+        13021: 13,
+        10701: 5,
+        12180: 12,
+        11550: 8,
+        13601: 16,
+        14215: 17,
+        10550: 4,
+        13501: 15,
+        11520: 7,
+        10801: 6,
+        13440: 14,
+        14304: 18,
+        14701: 20,
+        11757: 11})
 
 def get_prediction_features(order_id, session=Session):
     result = deliver.user_deliver(order_id=order_id, session=session)
@@ -41,9 +42,9 @@ def get_prediction_features(order_id, session=Session):
     weekday = result['order_date']['weekday']
     ship_mode = result['delivery_type']
     address = result['address']
-    original_code = address.split('/')[1]
+    original_code = int(address.split('/')[1])
 
-    postal_code = code_dic.get(original_code)
+    postal_code = code_dic[original_code]
     hurricane = 0
     badweather = 0
     
